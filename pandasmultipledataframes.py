7d8bad3d88d9b44cf681510b629b112b4414fb1b
raymondmar61 <raym61@hotmail.com>
2018-11-03 14:47:38 -0700

    upload

--- pandasmultipledataframes.py
+++ pandasmultipledataframes.py
@@ -64,13 +64,14 @@ print(results)
 '''      Month  Revenue  Target  Men  Women
 1  February      290     270   29     35
 '''
-#The column "id" in orders table or ordersdf.csv and in customers table or customersdf.csv are different.  The default merges are wrong.
+#The column "id" in orders table or ordersdf.csv and in customers table or customersdf.csv are different.  The default merges are wrong.  RM:  id column in orders is order_id.  id column in customers is customero_id.  Different id column name.
 #We solve the problem  use .rename to rename the columns for our merges. We rename the column id to customer_id, so that orders and customers have a common column for the merge.  pd.merge(orders, customers.rename(columns={'id': 'customer_id'}))
+#Use .rename to merge two DataFrames whose columns don't match.
 orders = pd.read_csv('ordersdf.csv')
-print(orders)
 products = pd.read_csv('productsdf.csv')
-print(products)
-orders_products = pd.merge(products, orders.rename(columns={"id":"customer_id"}))
+orders_products = pd.merge(products.rename(columns={"id55":"product_id"}), orders.rename(columns={"id55":"product_id"}))  #RM:  link products table product_id and orders table product_id as id55?
+#official solution: orders_products = pd.merge(orders, products.rename(columns={'id':'product_id'}))
+#orders_products = pd.merge(orders, products) #RM:  it appears .rename was unnecessary for my tables.  Codecademy must have changed the column names for the specific exercise.
 print(orders_products)
 '''
    product_id         description     ...      quantity   timestamp
@@ -85,4 +86,108 @@ print(orders_products)
 
 [8 rows x 7 columns]
 '''
-orders_products.to_csv('tempdelete.csv') 
+#We could use the keywords left_on and right_on to specify which columns we want to perform the merge on.  RM:  another way to merge two dataframes or two tables.
+#In the example below, the "left" table is the one that comes first (orders), and the "right" table is the one that comes second (customers). This syntax says that we should match the customer_id from orders to the id in customers.  pd.merge(orders, customers, left_on='customer_id', right_on='id').  RM:  better than .rename.
+#If we use this syntax, we'll end up with two columns called id, one from the first table and one from the second. Pandas won't let you have two columns with the same name, so it will change them to id_x and id_y.
+#Use the keyword suffixes. We can provide a list of suffixes to use instead of "_x" and "_y".  pd.merge(orders, customers, left_on='customer_id', right_on='id', suffixes=['_order', customer'])
+orders_productsleftright = pd.merge(orders, products, left_on="product_id", right_on="product_id", suffixes=["_orders","_products"])
+#solution orders_products = pd.merge(orders, products, left_on="product_id", right_on="id", suffixes=["_orders","_products"]) #RM:  Codecademy changed the column name on products table from product_id to id.
+print(orders_productsleftright)
+'''
+   order_id  customer_id  ...           description  price
+0         1            2  ...            doo-hickey      7
+1         5            3  ...            doo-hickey      7
+2         2            2  ...    whatcha-ma-call-it     10
+3         4            3  ...    whatcha-ma-call-it     10
+4         3            3  ...        thing-a-ma-jig      5
+5         7            1  ...        thing-a-ma-jig      5
+6         6            1  ...                 gizmo      3
+7         8            1  ...                 gizmo      3
+
+[8 rows x 7 columns]
+'''
+orders_productsleftright.to_csv('tempdelete.csv') #save orders_products to tempdelete.csv
+#If we wanted to combine the data from [tables where data is] missing from one of the tables, we could use an Outer Join. An Outer Join would include all rows from both tables, even if they don't match. Any missing values are filled in with None or nan (which stands for "Not a Number").  pd.merge(company_a, company_b, how='outer') #RM:  not left outer join or right outer join, yet
+store_a = pd.DataFrame([["hammer",12],["screwdriver",15],["nails",200],["screws",350],["saw",6],["duct tape",150],["wrench",12],["pvc pipe",54]], columns=["item","store_a_inventory"])
+store_b = pd.DataFrame([["hammer",6],["nails",250],["saw",6],["duct tape",150],["pvc pipe",54],["rake",10],["shovel",15],["wooden dowels",192]], columns=["item","store_b_inventory"])
+store_a_b_outer = pd.merge(store_a, store_b, how="outer")
+print(store_a_b_outer)
+'''
+             item  store_a_inventory  store_b_inventory
+0          hammer               12.0                6.0
+1     screwdriver               15.0                NaN
+2           nails              200.0              250.0
+3          screws              350.0                NaN
+4             saw                6.0                6.0
+5       duct tape              150.0              150.0
+6          wrench               12.0                NaN
+7        pvc pipe               54.0               54.0
+8            rake                NaN               10.0
+9          shovel                NaN               15.0
+10  wooden dowels                NaN              192.0
+'''
+#left merge we get all customers from Company A, and only customers from Company B who are also customers of Company A. pd.merge(company_a, company_b, how='left')
+#right merge we get all customers from Company B, and only customers from Company A who are also customers of Company B. pd.merge(company_a, company_b, how="right")
+store_a_b_left = pd.merge(store_a, store_b, how="left")
+print(store_a_b_left)
+'''
+          item  store_a_inventory  store_b_inventory
+0       hammer                 12                6.0
+1  screwdriver                 15                NaN
+2        nails                200              250.0
+3       screws                350                NaN
+4          saw                  6                6.0
+5    duct tape                150              150.0
+6       wrench                 12                NaN
+7     pvc pipe                 54               54.0
+'''
+store_b_a_left = pd.merge(store_b, store_a, how="left")
+print(store_b_a_left)
+'''
+            item  store_b_inventory  store_a_inventory
+0         hammer                  6               12.0
+1          nails                250              200.0
+2            saw                  6                6.0
+3      duct tape                150              150.0
+4       pvc pipe                 54               54.0
+5           rake                 10                NaN
+6         shovel                 15                NaN
+7  wooden dowels                192                NaN
+'''
+#When we need to reconstruct a single DataFrame from multiple smaller DataFrames, we can use the method pd.concat([df1, df2, df2, ...]). This method only works if all of the columns are the same in all of the DataFrames.  pd.concat([df1, df2]).  df1 and df2 have two columns name and email.
+bakery = pd.DataFrame([["cookie",2.5], ["brownie",3.5], ["slice of cake",4.75], ["slice of cheesecake",4.75], ["slice of pie",5]], columns=["item","price"])
+ice_cream = pd.DataFrame([["scoop of chocolate ice cream",3], ["scoop of vanilla ice cream",2.95], ["scoop of strawberry ice cream",3.05], ["scoop of cookie dough ice cream",3.25]], columns=["item","price"])
+menu = pd.concat([bakery, ice_cream])
+print(menu)
+'''
+                              item  price
+0                           cookie   2.50
+1                          brownie   3.50
+2                    slice of cake   4.75
+3              slice of cheesecake   4.75
+4                     slice of pie   5.00
+0     scoop of chocolate ice cream   3.00
+1       scoop of vanilla ice cream   2.95
+2    scoop of strawberry ice cream   3.05
+3  scoop of cookie dough ice cream   3.25
+'''
+
+#Working with Multiple DataFrames Quiz
+'''
+Which of the following best describes an inner merge?  A merge where only matching rows are included.  A merge where all rows are included, whether they match or not is an outer merge.
+What is the correct syntax for performing an outer merge on two Dataframes: df_one and df_two?  merged_df = pd.merge(df_one, df_two, how = 'outer'
+A veterinarians office is run by two vets, Greg and Susan, and stores each of their appointment data in separate DataFrames, called greg_appointments and susan_appointments respectively. These DataFrames have the same columns. If the vet office wanted to combine the two DataFrames into a single DataFrame called appointments_all which of the following commands would they use?  appointments_all = pd.concat([greg_appointments, susan_appointments])
+A veterinarian's office stores all of their data on pets and their owners in two dataframes: pets and owners. The owners dataframe has the columns 'id', 'first_name', 'last_name' and 'address'. The 'pets' dataframe has the columns id, name, owner_id, and type. If the office wanted to combine the two dataframes into one dataframe called pets_owers, what following code could work?  pets_owners = pd.merge(pets,owners.rename(columns = {'id':'owner_id'}))
+Which of the following best describes an outer merge?  A merge where all rows are included, whether they match or not.
+Which of the following best describes a left merge?  A merge where all rows from the first Dataframe are included, but only matching rows from the second Dataframe.
+Consider the following two Dataframes vets and appointments that store the appointment and vet data for a veterinarians office. Notice that some vets do not have any appointments scheduled, and some appointments do not yet have a vet assigned. If we wanted to combine the DataFrames into completed_appointments and only show the appointments that have had vets assigned to them, what code could we use?  completed_appointments = pd.merge(vets.rename(columns = {'id':'doctor_id'}), appointments, how = 'inner')
+'''
+
+#Page Visits Funnel Freeform Project
+#A funnel is a description of how many people continue to the next step of a multi-step process.  In this case, buying online.
+#RM:  data is crap.  Brain is fried 11/03/18 1430hrs.
+#How long is your merged DataFrame?  Use len.
+#print(len(visitscart))
+#How many of the timestamps are null for the column cart_time?
+#cart_time_null_count = cart[cart.cart_time.isnull()].count().reset_index()
+#print(cart_time_null_count)
